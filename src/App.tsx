b6deb6fbb52a3689c15fbe42115049f35891f674
Peterwood <156454307+shayra461@users.noreply.github.com>
2026-02-18 07:11:25 +0500

    Fix 404: Add basename to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/fresnaye-modernization">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/services" element={<ServicesPage />} />
